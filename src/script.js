3c54a1a4d097e8f9803e990db5d6620222d6c84a
Bilal Khatri <engrbilalfarooque@gmail.com>
2023-08-08 03:36:53 +0500

    hero done & navbar sticky

---
+++ src/script.js
@@ -0,0 +1,12 @@
+// window.addEventListener('scroll', () => {
+//     const header = document.querySelector('header');
+//     const navbar = document.querySelector('nav');
+    
+//     if (window.scrollY > header.offsetHeight) {
+//       navbar.classList.add('sticky');
+//       header.style.display = 'none';
+//     } else {
+//       navbar.classList.remove('sticky');
+//       header.style.display = 'block';
+//     }
+//   });
